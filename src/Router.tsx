454cd703b3884bbe1ac235998cb0372dadd11b0e
RAGAdox <rishitech882@gmail.com>
2024-12-27 16:33:51 +0530

    feat(routing): load component using async await

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,30 +6,38 @@ interface RouterProps {
 }
 const Router = ({ url = "/" }: RouterProps) => {
   const [currentHref, setCurrentHref] = React.useState<string>(url);
+  const [Component, setComponent] = React.useState<React.FC<any> | null>(null);
+  const [props, setProps] = React.useState<any>(null);
 
-  function getComponent(url: string) {
+  async function getComponent(url: string) {
     const Component =
       url === "" || url === "/"
         ? React.lazy(() => import("./pages/Home"))
         : React.lazy(() => import("./pages/About"));
-
     return Component;
   }
   useEffect(() => {
     const handlePopState = () => {
       setCurrentHref(window.location.pathname);
     };
+
     window.addEventListener("popstate", handlePopState);
-    return () => {
-      window.removeEventListener("popstate", handlePopState);
-    };
+    return () => window.removeEventListener("popstate", handlePopState);
   }, []);
 
-  return (
-    <Suspense fallback={<Loader />}>
-      {React.createElement(getComponent(currentHref))}
-    </Suspense>
-  );
+  useEffect(() => {
+    const loadComponent = async (href: string) => {
+      const Component = await getComponent(href);
+
+      setComponent(Component);
+    };
+    loadComponent(currentHref);
+  }, [currentHref]);
+
+  if (!Component) {
+    return <Loader />;
+  }
+  return <Suspense fallback={<Loader />}>{<Component />}</Suspense>;
 };
 
 export default Router;
