5eb479b1c620f290fbe7245e41e7f13a1b281295
Rishi Mukherjee <rishitech882@gmail.com>
2025-02-08 20:49:47 +0530

    fix(background): apply styling based on aspect ratio on SVG

--- apps/portfolio/app/components/Background/Backgroud.tsx
+++ apps/portfolio/app/components/Background/Backgroud.tsx
@@ -1,17 +1,22 @@
 import { Box } from "@radix-ui/themes";
 
+import { useEffect, useState } from "react";
 import SVGComponent from "../../bg.svg";
 const Backgroud = () => {
+  const [aspectRatio, setAspectRatio] = useState<number>();
+
+  useEffect(() => {
+    const handleResize = () => {
+      const ratio = window.innerWidth / window.innerHeight;
+      setAspectRatio(ratio);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      {/* <Box
-        minHeight="100svh"
-        minWidth="100vw"
-        position="fixed"
-        style={{
-          backgroundImage: 'url("/assets/bg.svg")',
-        }}
-      ></Box> */}
       <Box
         position="fixed"
         top="0"
@@ -26,10 +31,12 @@ const Backgroud = () => {
             position: "absolute",
             top: 0,
             left: 0,
-            width: "100%",
             fill: "var(--accent-9)",
             opacity: 0.1,
           }}
+          {...(aspectRatio && aspectRatio > 1
+            ? { width: "100%" }
+            : { height: "100%" })}
         />
       </Box>
 
