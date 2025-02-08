c56d355a31e2bbcfcf066c70a1628fb333bfce1c
Rishi Mukherjee <rishitech882@gmail.com>
2025-02-09 03:04:56 +0530

    chore(turborepo): update configuration for local setup

--- apps/zenith-ui/vite.config.ts
+++ apps/zenith-ui/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      port: parseInt(env.VITE_PORT || "3002", 10),
+    },
+    plugins: [react()],
+  };
+});
